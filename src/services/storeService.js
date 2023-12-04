74a977ae38422053622f7f2bf9bff23401d82cff
강한나 <hanna0175@gmail.com>
2023-12-04 13:39:15 +0900

    🦺 add: 기간설정 조건문 수정

--- src/services/storeService.js
+++ src/services/storeService.js
@@ -18,11 +18,19 @@ exports.getStores = async function (query) {
   const conditions = {};
 
   if (title) conditions.title = { $regex: new RegExp(title, 'i') };
-  // if (categoryId) conditions.categoryId = categoryId;
-  // if (adultVerification !== undefined)
-  // conditions.adultVerification = adultVerification;
-  if (startDate) conditions.startDate = { $lte: Number(startDate) };
-  if (endDate) conditions.endDate = { $gte: Number(endDate) };
+  if (startDate || endDate) {
+    conditions.$or = [
+      { startDate: { $gte: Number(startDate), $lte: Number(endDate) } },
+      { endDate: { $gte: Number(startDate), $lte: Number(endDate) } },
+      {
+        $and: [
+          { startDate: { $lte: Number(startDate) } },
+          { endDate: { $gte: Number(endDate) } },
+        ],
+      },
+    ];
+  }
+
   if (locationId) conditions.locationId = { $in: [locationId] };
   const stores = await Store.find(conditions)
     .skip((page - 1) * perPage)
